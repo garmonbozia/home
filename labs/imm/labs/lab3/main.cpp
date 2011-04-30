bb0eecc3ffec75e1fd14a411ec117b32f52e6275
garmonbozia <smpcln@gmail.com>
2011-05-01 02:28:15 +0400

    imm.lab3 added

---
+++ labs/imm/labs/lab3/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#define DIMENTION_THREE
+#include "jak_grf.h"
+
+using std::cout;
+using std::endl;
+
+int main ( )
+{
+#ifdef DIMENTION_THREE
+	jak_grf graph( "5.ig", "5_border", "5_groupet" );
+#else
+	jak_grf graph( "6.ig", "6_border", "6_groupet" );
+#endif
+	graph.initial( );
+#ifndef DIMENTION_THREE
+	graph.show_adjacency_matrix( );
+	graph.show_coord( );
+	graph.show_groups( );
+	graph.show_bordered( );
+#endif
+
+	max_min_t<double> max_min_size;
+	max_min_size = graph.max_min_group_size( );
+	cout << "max group size= " << max_min_size.max;
+	cout << "(" << max_min_size.index_max << ")" << endl;
+	cout << "min group size= " << max_min_size.min;
+	cout << "(" << max_min_size.index_min << ")" << endl;
+
+	max_min_size = graph.max_min_bordered_group_size( );
+	cout << "max bordered group size= " << max_min_size.max;
+	cout << "(" << max_min_size.index_max << ")" << endl;
+	cout << "min bordered group size= " << max_min_size.min;
+	cout << "(" << max_min_size.index_min << ")" << endl;
+
+	max_min_size = graph.max_min_connected_or_bordered_group_size( );
+	cout << "max connected or bordered group size= " << max_min_size.max;
+	cout << "(" << max_min_size.index_max << ")" << endl;
+	cout << "min connected or bordered group size= " << max_min_size.min;
+	cout << "(" << max_min_size.index_min << ")" << endl;
+
+	cout << "connective group number= " << graph.connective_group_number( ) << endl;
+	return( 0 );
+}
