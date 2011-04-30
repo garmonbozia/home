19e3f704d1a5473c1b62da970798d9ff5d2397d3
garmonbozia <smpcln@gmail.com>
2011-05-01 02:02:06 +0400

    get_counter added

--- tools/glistfm/glistfm.sh
+++ tools/glistfm/glistfm.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+
 USERNAME=$1
+API_KEY=b25b959554ed76058ac220b7b2e0a026
+
+get_counter_value()
+{
+    curl -s "http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${USERNAME}&api_key=${API_KEY}" |\
+    grep -E "^\ +<playcount>.*</playcount>$" |\
+    sed -r -e 's/<(\/)?playcount>//g' -e 's/^(\ )+(.*)*$/\2/g'
+}
+
 songs=`curl -s http://ws.audioscrobbler.com/1.0/user/${USERNAME}/recenttracks.rss |\
 grep -E "^\ +<title>.*</title>$" |\
 sed -r -e 's/<(\/)?title>//g' -e 's/^(\ )+(.*)*$/\2/g'`
@@ -19,6 +29,10 @@ case "$2" in
     ;;
     -a) #last 10 songs
         echo "$songs"
+        exit
+    ;;
+    -c) #counter
+        get_counter_value
         exit
     ;;
 esac
