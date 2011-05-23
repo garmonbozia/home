e6582ffa2dde4cac9a9725f7088438db364e1331
garmonbozia <smpcln@gmail.com>
2011-05-24 01:23:43 +0400

    m

--- tools/m100/m100.pl
+++ tools/m100/m100.pl
@@ -1,75 +1,166 @@
-:- include('../../libs/listlib.pl').
+:- include(  '../../libs/listlib.pl'  ).
 
-listToNumberAll([First | Tail], Number) :-
-	((
-	First >= 0,
-	listToNumber([First | Tail], Number)
-	);(
-	First < 0,
-	FirstNew is -First,
-	listToNumber([FirstNew | Tail], Num),
-	Number is -Num
-	)).
+digits( [0,1,2,3,4,5,6,7,8,9] ).
+
+listToNumberAll( [First | Tail], Number ) :-
+	( First >= 0
+    , listToNumber( [First | Tail], Number )
+    )
+    ;
+    ( First < 0
+    , FirstNew is -First
+    , listToNumber( [FirstNew | Tail], Num )
+    , Number is -Num
+	).
 
 /**************************
  * local n2l - first num +-
  **************************/
-n2l(Number, List) :-
-	numberToList(Number, L),
+n2l( Number, List ) :-
+	numberToList( Number, L ),
 	L = [H | Tail],
-	(
-	(H = 0, List = L)
-	;
-	(H =\= 0, 
-		(List = L; 
-		HNew is -H, List = [HNew | Tail]
+	( ( H = 0
+      , List = L
+      )
+	  ;
+	  ( H =\= 0
+      , ( List = L
+        ; HNew is -H
+        , List = [HNew | Tail]
 		)
 	)
 	).
 
-operator(Operator) :-
-	member(Operator, [+, -, *, /]).
+expression( List, X ) :-
+    expressionH(List, [], X).
 
-formula(List, X) :-
-	formula(List, [], X).
-formula([], [A], A).
-formula([H | Tail], A, X) :-
-	formula(Tail, [H | A], X).
-formula(Tail, [An, Am | A], X) :-
-	operator(Operator),
-	Form =.. [Operator, Am, An],
-	formula(Tail, [Form | A], X).
+expressionH( [], [A], A ).
+expressionH( [H | Tail], A, X ) :-
+    expressionH( Tail, [H | A], X ).
+expressionH( Head, [An, Am | A], X ) :-
+    member( Operator, [+, -, *, /] ),
+    Form =.. [Operator, Am, An],
+    expressionH( Head, [Form | A], X ).
 
-makeAnsList(NumList, Result, Formula) :-
-	shatter(NumList, SubList),
-	maplist(listToNumberAll, SubList, List),
-	formula(List, Formula),
-	catch(Result_t is Formula, _, fail),
-	Result =:= Result_t.
+/*
+expression( List, Expression ) :-
+    reverse( List, ListRev),
+    expressionH( ListRev, Expression).
+expressionH( [X1, X2 | Tail], Acc ) :-
+     member( Operator, [+, -, *, /] ),
+     F =.. [Operator, X2, X1],
+     expressionH( [F | Tail], Acc ).
+expressionH( [Expr], Expr ).
+*/
 
-makeAns(Num, Result, Formula) :-
-	n2l(Num, NumList),
-	makeAnsList(NumList, Result, Formula).
+%% makeAnsList( +NumList, +Result, -Formula )
+%
+% generate Formula (got from Numlist), that equal Result
+makeAnsList( NumList, Result, Formula ) :-
+	shatter( NumList, SubList ),
+	maplist( listToNumberAll, SubList, List ),
+	expression( List, Formula ),
+	% ignoring evaluation error: `zero_divisor'
+    catch( Result is Formula, _, false ).
 
-write_num(Num, OS) :-
-	forall(member(X, Num),
-		write(OS, X)),
-	nl(OS),
-	writeln(Num).
+unsolvebleNumList( NumList, Result ) :-
+	\+makeAnsList( NumList, Result, _ ).
 
-main(N) :-
-	open('m100.log', write, OS),
-	(
-	length(Nums, N),
-	get_set(NumList, Nums,
-			[0,1,2,3,4,5,6,7,8,9]),
-	listToNumber(NumList, Num),
-	\+makeAns(Num, 100, _),
-	write_num(NumList, OS),
+solvebleNumList( NumList, Result ) :-
+    makeAnsList( NumList, Result, _ ),
+    !.
+
+solveNum( Num, Result, Formula ) :-
+	n2l( Num, NumList ),
+	makeAnsList( NumList, Result, Formula ).
+
+solvebleNum( Num, Result ) :-
+    solveNum( Num, Result, _ ).
+
+unsolvebleNum( Num, Result ) :-
+    \+solveNum( Num, Result, _ ).
+
+writeNumList( NumList, OS ) :-
+	forall( member( X, NumList ), write( OS, X ) ),
+	nl( OS ).
+
+m100( Num, Formula ) :-
+    solveNum( Num, 100, Formula).
+
+unsolvebleFixedLength( N, NumList ) :-
+    length( Nums, N ),
+    digits( Digits ),
+    get_set( NumList, Nums, Digits ),
+    listToNumber( NumList, Number ),
+    num_length( Number, N ),
+    unsolvebleNumList( NumList, 100 ).
+
+%% formulasCount( +NumList, -Count )
+%
+% counting quantity of all formulas 
+% possible to create from NumList
+
+formulasCount( N, Count ) :-
+    length( List, N ),
+    aggregate( count
+             , F^( shatter( List, SubList )
+                 , maplist( listToNumberAll, SubList, L )
+                 , expression( L, F )
+                 )
+            , Count
+             ).
+
+allFixedLength( N ) :-
+    open( 'm100_all.log', write, OS ),
+    (
+        length( Nums, N ),
+        digits( Digits ),
+        get_set( NumList, Nums, Digits ),
+        listToNumber( NumList, Number ),
+        num_length( Number, N ),
+        format( OS, 'num: ~w, solved: ', [Number] ),
+        %write( OS, 'num: '),
+        %write( OS, Number ),
+        %write( OS, ', solved: ' ),
+        (
+        (
+            unsolvebleNumList( NumList, 100 ),
+            write( OS, '-' )
+        )
+            ;
+        (
+            \+unsolvebleNumList( NumList, 100 ),
+            write( OS, '+' )
+        )
+        ),
+        nl( OS ),
+        fail
+    ;
+        close( OS )
+    ).
+
+allUnsolvebleFixedLength( N ) :-
+    open( 'm100.unsolveble.log', write, OS ),
+    (
+        unsolvebleFixedLength( N, NumList ),
+        writeNumList( NumList, OS ),
+        writeln( NumList ),
+        fail
+    ;
+        close( OS )
+    ).
+
+main( N ) :-
+	open( 'm100.log', write, OS ),
+	( 
+	length( Nums, N ),
+    digits( Digits ),
+	get_set( NumList, Nums, Digits ),
+    unsolvebleNumList( NumList, 100 ),
+	writeNumList( NumList, OS ),
+	writeln( NumList ),
 	fail
 	;
-	close(OS)
+	close( OS )
 	).
 
-% 	usage
-% allFormulas([], L, X, R), format('~w~t~30| = ~w~n', [X, R]), fail.
--- tools/m100/m100.sh
+++ tools/m100/m100.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-DIRECTORY='./'
+DIRECTORY='.'
 OUTPUT_FILE=${DIRECTORY}/m100_output
 SOURCE_FILE=${DIRECTORY}/m100.pl
 NUMBER=$1
-#LENGTH=$((`echo $NUMBER | wc -c`-1))
 
 echo -n "" > $OUTPUT_FILE
-swipl -q -t halt  -s ${SOURCE_FILE} -g "N = $NUMBER, R = 100, makeAns(N, R, X), open('$OUTPUT_FILE', append, OS), format(OS, '~w =  ~w~n', [R, X]), close(OS), false"
+swipl -q -t halt  -s ${SOURCE_FILE} -g "\
+    Num = $NUMBER,\
+    m100(Num, X),\
+    open('$OUTPUT_FILE', append, OS),\
+    format(OS, '~w  = 100~n', [X]),\
+    close(OS),\
+    false"
 
-if [ "$2" == "-n" ] 
+if [ "$2" != "-n" ] 
 then
 	echo -n "$1:"
 	[ $((`cat $OUTPUT_FILE | wc -l`)) == 0 ] && echo -e "\t \033[1;31m not this one \033[0;37m" || echo -e "\t \033[1;32m oook \033[0;37m"
 	cat $OUTPUT_FILE
+    rm $OUTPUT_FILE
 fi
  
